336133d6986e1287427bab740ab93374ec834328
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-18 15:39:49 +0600

    Add tests for SessionLoad middleware and routes function
    Adds test for SessionLoad middleware to verify it returns http.Handler type. Creates routes_test.go with test to verify routes function returns *chi.Mux type.

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -19,3 +19,17 @@ func TestNoSurf(t *testing.T) {
 		t.Error(fmt.Sprintf("type is not http.Handler, but is %T", v))
 	}
 }
+
+func TestSessionLoad(t *testing.T) {
+
+	var myH myHandler
+
+	h := NoSurf(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Error(fmt.Sprintf("type is not http.Handler, but is %T", v))
+	}
+}
