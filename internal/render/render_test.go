1605752630e5a948e05692d035f1b343c60cb887
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-27 13:57:01 +0600

    Add RenderTemplate tests and return error handling
    Implements TestRenderTemplate to verify template rendering for valid and non-existent templates. Adds myWriter test helper implementing http.ResponseWriter interface. Updates

--- internal/render/render_test.go
+++ internal/render/render_test.go
@@ -24,6 +24,33 @@ func TestAddDefaultData(t *testing.T) {
 	}
 }
 
+func TestRenderTemplate(t *testing.T) {
+	pathToTemplates = "./../../templates"
+	tc, err := CreateTemplateCache()
+	if err != nil {
+		t.Error(err)
+	}
+
+	app.TemplateCache = tc
+
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+
+	var ww myWriter
+
+	err = RenderTemplate(&ww, r, "home.page.gohtml", &models.TemplateData{})
+	if err != nil {
+		t.Error("error writing template to browser")
+	}
+
+	err = RenderTemplate(&ww, r, "non-existent.page.gohtml", &models.TemplateData{})
+	if err == nil {
+		t.Error("rendered template that does not exist")
+	}
+}
+
 func getSession() (*http.Request, error) {
 	r, err := http.NewRequest("GET", "/some-url", nil)
 	if err != nil {
