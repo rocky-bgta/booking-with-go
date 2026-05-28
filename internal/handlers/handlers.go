9525b6a8834d93f0b85879f276edcc3191ced87f
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-28 13:16:01 +0600

    refactor(forms): remove net/http dependency and fix form initialization
    - Remove unused `net/http` import from forms.go
    - Change `New()` to accept `url.Values` directly instead of `*http.Request`
    - Update tests to use `r.PostForm` instead of `r.Form` for form initialization
    - Update handlers to construct forms with `r.PostForm`

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -88,7 +88,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	//form.Has("last_name", r)
 
 	form.Required("first_name", "last_name", "email")
-	form.MinLength("first_name", 3, r)
+	form.MinLength("first_name", 3)
 	form.IsEmail("email")
 
 	if !form.Valid() {
