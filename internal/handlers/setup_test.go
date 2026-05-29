d8a319fb850cbc04974c7eefe84fca44032679cb
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-29 11:59:00 +0600

    test(handlers): initialize InfoLog and ErrorLog in test setup
    Add InfoLog and ErrorLog fields to AppConfig in getRoutes() to satisfy
    handler dependencies that require loggers to be set before processing
    requests.

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -29,6 +30,9 @@ func getRoutes() http.Handler {
 
 	// change this to true when in production
 	app.InProduction = false
+
+	app.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// set up the session
 	session = scs.New()
