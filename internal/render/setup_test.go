ba89f5acaf970372645b697b0df3be9488e8d436
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-29 13:22:35 +0600

    chore(infra): add docker-compose for PostgreSQL and fix render test setup
    - Add docker-compose.yml with PostgreSQL 14 service on port 5432
    - Configure isolated bridge network (172.30.0.0/24) and named volume
    - Initialize InfoLog and ErrorLog in render package TestMain to match
      AppConfig requirements

--- internal/render/setup_test.go
+++ internal/render/setup_test.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"encoding/gob"
+	"log"
 	"net/http"
 	"os"
 	"testing"
@@ -22,6 +23,9 @@ func TestMain(m *testing.M) {
 
 	// change this to true when in production
 	testApp.InProduction = false
+
+	testApp.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	testApp.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// set up the session
 	session = scs.New()
