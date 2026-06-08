248a43ffb7d663684cc9d3cb6ce9f5c0454c9656
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-06-08 10:06:22 +0600

    feat(db,handlers): add InsertReservation to persist booking data
    - Add InsertReservation method to DatabaseRepo interface
    - Implement InsertReservation in postgresDBRepo with context timeout
    - Add strconv and time imports to handlers for reservation processing

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/rocky_bgta/booking-with-go/internal/config"
 	"github.com/rocky_bgta/booking-with-go/internal/driver"
@@ -68,18 +70,42 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	//2020-12-28 01/02 03:04:05PM '06 -0700
+	layout := "2006-01-02"
+
+	starDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
 	// extract data from the form and create a reservation struct
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: starDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
-	//form.Has("first_name", r)
-	//form.Has("last_name", r)
-
 	form.Required("first_name", "last_name", "email")
 	form.MinLength("first_name", 3)
 	form.IsEmail("email")
@@ -91,6 +117,12 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Form: form,
 			Data: data,
 		})
+		return
+	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 		return
 	}
 
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/rocky_bgta/booking-with-go/internal/models"
+
 type DatabaseRepo interface {
 	AllUser() bool
+
+	InsertReservation(res models.Reservation) error
 }
